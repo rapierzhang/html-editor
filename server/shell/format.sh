6cb4607e9f4700a142fb32eaada4206954e73c13
zhangmingyang <zhangmingyang@sogou-inc.com>
2020-05-08 18:08:55 +0800

    用px2rem工具更改

--- server/shell/format.sh
+++ server/shell/format.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+px2rem $1/css/index.css -u 23.4375
+mv $1/css/index.debug.css $1/css/index.build.css
+
 prettier --single-quote --write "$1/*.html"
 prettier --single-quote --write "$1/**/*.{js,css}"
