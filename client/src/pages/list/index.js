96e9f4986e61f6c18ceebb211676c484facb7680
zhangmingyang <zhangmingyang@sogou-inc.com>
2020-05-12 12:57:08 +0800

    模糊查询前端

--- client/src/pages/list/index.js
+++ client/src/pages/list/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { listGet } from './actions';
 import { ListItem } from './components';
-import './list.scss';
 import { utils } from '../../common';
 import { Page } from '../../component/common';
+import './list.scss';
 
 class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            searchText: '',
+
             listData: utils.defaultData,
         };
     }
@@ -17,19 +19,35 @@ class List extends Component {
         this.listGet();
     }
 
-    listGet(pn = 1, ps = 5) {
+    // ^^^^^^
+    listGet(pn = 1, ps = 6) {
         utils.fetchLoading(this, 'listData');
         listGet({ pn, ps })
             .then(listData => utils.fetchSucc(this, 'listData', listData))
             .catch(() => utils.fetchErr(this, 'listData'));
     }
 
-    newHtml() {
+    // 新建页面
+    newPage() {
         window.open('editor');
     }
 
+    // 更改页码
     pageChange(index) {
         this.listGet(index);
+    }
+
+    // 输入搜索内容
+    searchChange(e) {
+        this.setState({
+            searchText: e.target.value,
+        });
+    }
+
+    // 模糊查询
+    search() {
+        const { searchText } = this.state;
+        console.error(searchText)
     }
 
     render() {
@@ -44,9 +62,20 @@ class List extends Component {
                 {/*------ 头部 ------*/}
                 <div className='header'>
                     <div className='title'>页面生成器</div>
-                    <div className='btn-box'>
-                        <div className='button primary' onClick={this.newHtml.bind(this)}>
-                            新建
+                    <div className='right-box'>
+                        <div className='search-box'>
+                            <input
+                                className='search-input'
+                                type='text'
+                                placeholder='请输入标题或简介的关键字'
+                                onChange={this.searchChange.bind(this)}
+                            />
+                            <div className='search-btn' onClick={this.search.bind(this)}>搜索</div>
+                        </div>
+                        <div className='btn-box'>
+                            <div className='button primary' onClick={this.newPage.bind(this)}>
+                                新建
+                            </div>
                         </div>
                     </div>
                 </div>
